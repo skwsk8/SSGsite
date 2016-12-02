78e14a197fae5a967455c6758017b3fc3da155f3
skwsk8 <skwsk8@gmail.com>
2016-12-02 16:29:50 -0500

    revamp
    added Psyche Soldier VR 12-2-2016

--- ssg_scripts_shared.js
+++ ssg_scripts_shared.js
@@ -1,54 +1,5 @@
 // JavaScript - Single Step Games, LLC (c)2016
 
-// Menu Functions
-var delayTimerDuration=300;
-var delayTimer=0;
-var menuDropDown=0;
-
-function showMenuDropDown(menuId)
-{
-	cancelMenuHideDelayTimer();
-	if (menuDropDown) menuDropDown.style.visibility="hidden";
-	menuDropDown=document.getElementById(menuId);
-	menuDropDown.style.visibility="visible";
-}
-
-function hideMenuDropDown()
-{
-	if (menuDropDown) menuDropDown.style.visibility="hidden";
-}
-
-function startMenuHideDelayTimer()
-{
-	delayTimer=window.setTimeout(hideMenuDropDown,delayTimerDuration);
-}
-
-function cancelMenuHideDelayTimer()
-{
-	if (delayTimer)
-	{
-		window.clearTimeout(delayTimer);
-		delayTimer=null;
-	}
-}
-
-// Load Videos for non-mobile
-function loadVideos()
-{
-	if (screen.width >= 800)
-	{
-		document.getElementById("vidEM2").src="https://www.youtube.com/embed/0MyK8I3b-fA";
-		document.getElementById("vidDCUO").src="https://www.youtube.com/embed/ywt5SqXxpSI";
-		document.getElementById("vidCubis").src="https://www.youtube.com/embed/pVLC2CicoEg";
-		document.getElementById("vidPP").src="https://www.youtube.com/embed/WxKuZTBQLwI";
-		document.getElementById("vidWAW").src="https://www.youtube.com/embed/G0_-Oe6yhco";
-		document.getElementById("vidGW").src="https://www.youtube.com/embed/gPD_wC70OY0";
-		document.getElementById("vidNWN").src="https://www.youtube.com/embed/ashgP4YMdJw";
-		document.getElementById("vidCOD4").src="https://www.youtube.com/embed/33wsPV6dYHI";
-		document.getElementById("vidRR").src="https://www.youtube.com/embed/ErGoQz3c3hU";
-	}
-}
-
 // Footer Function
 function getFooterDate(footerText,originYear)
 {
@@ -56,10 +7,10 @@ function getFooterDate(footerText,originYear)
 	var d = new Date();
 	if (d.getFullYear() == originYear)
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+" Single Step Games, LLC. All rights reserved.";
+	footerText.innerHTML="Copyright &copy; "+originYear+" Single Step Games, LLC. SINGLE STEP GAMES, PSYCHE SOLDIER, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 	else
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. All rights reserved.";
+	footerText.innerHTML="Copyright &copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. SINGLE STEP GAMES, PSYCHE SOLDIER, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 }
