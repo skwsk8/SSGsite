e15f0aea839b98338060810a50054bd1335819df
skwsk8 <skwsk8@gmail.com>
2018-09-20 20:46:10 -0400

    Add files via upload
    break stuff revamp

--- ssg_scripts_shared.js
+++ ssg_scripts_shared.js
@@ -7,10 +7,10 @@ function getFooterDate(footerText,originYear)
 	var d = new Date();
 	if (d.getFullYear() == originYear)
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+" Single Step Games, LLC. SINGLE STEP GAMES, PSYCHE SOLDIER, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
+	footerText.innerHTML="Copyright &copy; "+originYear+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 	else
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. SINGLE STEP GAMES, PSYCHE SOLDIER, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
+	footerText.innerHTML="Copyright &copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 }
