92da25b1424281b8cd8c873c76977b812c4bc192
skwsk8 <skwsk8@gmail.com>
2023-04-03 02:06:54 -0400

    revamp

--- ssg_scripts_shared.js
+++ ssg_scripts_shared.js
@@ -1,5 +1,22 @@
 // JavaScript - Single Step Games, LLC (c)2016
 
+function carousel() {
+	var i;
+	var x = document.getElementsByClassName("quotes");
+	for(i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[Math.floor(Math.random()*x.length)].style.display = "block";
+	setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+function dynamicVideo() {
+	if(window.innerWidth >= 1080){
+		const dynamicVideo=document.getElementById("dynamicVideo");
+		dynamicVideo.innerHTML='<source src="Videos/BreakStuffVrWebLoop.mp4" type="video/mp4">'
+	}
+}
+
 // Footer Function
 function getFooterDate(footerText,originYear)
 {
@@ -7,10 +24,10 @@ function getFooterDate(footerText,originYear)
 	var d = new Date();
 	if (d.getFullYear() == originYear)
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
+	footerText.innerHTML="&copy; "+originYear+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 	else
 	{
-	footerText.innerHTML="Copyright &copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
+	footerText.innerHTML="&copy; "+originYear+"-"+d.getFullYear()+" Single Step Games, LLC. SINGLE STEP GAMES, BREAK STUFF VR, and PSYCHE SOLDIER VR are trademarks of Single Step Games, LLC. All rights reserved. All other trademarks and trade names are property of their respective owners.";
 	}
 }
